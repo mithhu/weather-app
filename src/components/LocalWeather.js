5d74599511d90596b0c1a3d134c206378d41d3e7
mithhu <mithhu666@gmail.com>
2019-05-26 00:51:46 +0600

    Local weather setup

--- src/components/LocalWeather.js
+++ src/components/LocalWeather.js
@@ -1,12 +1,50 @@
 import React from "react";
 import LocalWeatherView from "./LocalWeatherView";
+const API_KEY = "f64e34286ec3110a98b3b72018975f71";
 
 class LocalWeather extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    error: undefined
+  };
+  getWeather = () => {
+    navigator.geolocation.getCurrentPosition(async position => {
+      const latitude = await position.coords.latitude;
+      const longitude = position.coords.longitude;
+      const api_call = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+      );
+      const data = await api_call.json();
+      if (data) {
+        this.setState({
+          temperature: data.main.temp,
+          city: data.name,
+          error: ""
+        });
+      } else {
+        this.setState({
+          temperature: "",
+          city: "",
+          error: "Sorry! Your location is not available"
+        });
+      }
+    });
+  };
+
+  componentDidMount() {
+    this.getWeather();
+  }
+
   render() {
     return (
       <div>
         <p>Local Weather</p>
-        <LocalWeatherView />
+        <LocalWeatherView
+          temperature={this.state.temperature}
+          city={this.state.city}
+          error={this.state.error}
+        />
       </div>
     );
   }
--- src/components/LocalWeatherView.js
+++ src/components/LocalWeatherView.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-const LocalWeatherView = props => <p>Local Weather View</p>;
+const LocalWeatherView = props => (
+  <div>
+    {!props.city && <p>Your Location</p>}
+    {props.city && (
+      <div>
+        <p>
+          Your Location is {props.city} and the temperature is{" "}
+          {props.temperature} degree celcius
+        </p>
+      </div>
+    )}
+    {props.error && <p>{props.error}</p>}
+  </div>
+);
 
 export default LocalWeatherView;
